11f39d32af6cb1385353ea4a20dd762929e027be
Maik Schneider <mas@xima.de>
2024-05-27 15:34:06 +0200

    feat: install and configure typo3 testingframework

---
+++ Tests/Unit/Utility/LogParserUtilityTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Xima\XimaTypo3Mailcatcher\Tests\Utility;
+
+use Xima\XimaTypo3Mailcatcher\Utility\LogParserUtility;
+use PHPUnit\Framework\TestCase;
+
+class LogParserUtilityTest extends TestCase
+{
+
+    public function testLoadAndGetMessages()
+    {
+
+    }
+
+    public function testLoadMessages()
+    {
+
+    }
+
+    public function testDeleteMessages()
+    {
+
+    }
+
+    public function testDeleteMessageByFilename()
+    {
+
+    }
+
+    public function testGetPublicPath()
+    {
+
+    }
+
+    public function testGetMessageByFilename()
+    {
+
+    }
+
+    public function testGetTempPath()
+    {
+
+    }
+}
