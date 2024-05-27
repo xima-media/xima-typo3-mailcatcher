a7db4f4da0f41f6f2c5c059e77a97b9b4ab6813f
Maik Schneider <mas@xima.de>
2024-05-27 17:54:24 +0200

    feat: add test for environment tool example mail

--- Tests/Unit/Utility/LogParserUtilityTest.php
+++ Tests/Unit/Utility/LogParserUtilityTest.php
@@ -2,6 +2,7 @@
 
 namespace Xima\XimaTypo3Mailcatcher\Tests\Unit\Utility;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 use Xima\XimaTypo3Mailcatcher\Utility\LogParserUtility;
 
@@ -9,34 +10,30 @@ class LogParserUtilityTest extends UnitTestCase
 {
     protected LogParserUtility $subject;
 
-    public function testLoadAndGetMessages(): void
+    public function testRunWithEnvironmentToolMail(): void
     {
+        $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_mbox_file'] = '/var/www/html/Tests/Fixtures/mail-env-tool.log';
 
-    }
+        $this->subject->run(false);
 
-    public function testLoadMessages(): void
-    {
+        self::assertDirectoryExists($this->subject::getTempPath() . 'd41d8cd98f00b204e9800998ecf8427e');
+        self::assertFileExists($this->subject::getTempPath() . '1716817329-74be16979710d4c4e7c6647856088456.json');
+        self::assertJson(file_get_contents($this->subject::getTempPath() . '1716817329-74be16979710d4c4e7c6647856088456.json'));
 
-    }
-
-    public function testDeleteMessages(): void
-    {
-
-    }
-
-    public function testDeleteMessageByFilename(): void
-    {
-
-    }
-
-    public function testGetPublicPath(): void
-    {
-
-    }
-
-    public function testGetMessageByFilename(): void
-    {
-
+        $messages = $this->subject->loadAndGetMessages();
+        self::assertCount(1, $messages);
+        self::assertEquals('d41d8cd98f00b204e9800998ecf8427e', $messages[0]->messageId);
+        self::assertIsObject($messages[0]->date);
+        self::assertStringStartsWith('Test TYPO3 CMS mail delivery from site', $messages[0]->subject);
+        self::assertEquals('hello@example.com', $messages[0]->from);
+        self::assertEquals('TYPO3 CMS install tool', $messages[0]->fromName);
+        self::assertEquals('recipent@example.com', $messages[0]->to);
+        self::assertEquals(0, strlen($messages[0]->toName));
+        self::assertStringContainsString('Hey TYPO3 Administrator', $messages[0]->bodyPlain);
+        self::assertStringContainsString('<table', $messages[0]->bodyHtml);
+        self::assertEmpty($messages[0]->ccRecipients);
+        self::assertEmpty($messages[0]->bccRecipients);
+        self::assertEmpty($messages[0]->attachments);
     }
 
     public function testGetTempPath(): void
@@ -49,5 +46,10 @@ class LogParserUtilityTest extends UnitTestCase
     {
         parent::setUp();
         $this->subject = new LogParserUtility();
+    }
+
+    protected function tearDown(): void
+    {
+        GeneralUtility::rmdir(LogParserUtility::getTempPath(), true);
     }
 }
