d8a1c2223a6bc92c490ee73b8579cbe6fcde304a
Maik Schneider <mas@xima.de>
2024-05-31 16:16:44 +0200

    feat: add data provider for functional tests

--- Tests/Functional/Utility/LogParserUtilityTest.php
+++ Tests/Functional/Utility/LogParserUtilityTest.php
@@ -2,6 +2,7 @@
 
 namespace Xima\XimaTypo3Mailcatcher\Tests\Functional\Controller;
 
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mime\Address;
 use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Mail\FluidEmail;
@@ -25,29 +26,111 @@ class LogParserUtilityTest extends FunctionalTestCase
         'typo3conf/ext/xima_typo3_mailcatcher',
     ];
 
-    public function testEmailEncoding(): void
+    /**
+     * @return array<int, array<string, string>>[]
+     */
+    public static function mailDataProvider(): array
     {
-        $this->createTestMail();
+        return [
+            [
+                [
+                    'to' => 'contact@example.org',
+                    'toName' => 'Contact',
+                    'from' => 'hello-world@example.com',
+                    'fromName' => 'Test',
+                    'subject' => 'TYPO3 loves you - here is why',
+                    'template' => 'TestMailTemplate',
+                    'format' => FluidEmail::FORMAT_BOTH,
+                ],
+            ],
+            [
+                [
+                    'to' => 'contact@example.org',
+                    'toName' => 'Contact',
+                    'from' => 'hello-world@example.com',
+                    'fromName' => 'Test',
+                    'subject' => 'TYPO3 loves you - here is why',
+                    'template' => 'TestMailTemplate',
+                    'format' => FluidEmail::FORMAT_PLAIN,
+                ],
+            ],
+            [
+                [
+                    'to' => 'contact@example.org',
+                    'toName' => 'Contact',
+                    'from' => 'hello-world@example.com',
+                    'fromName' => 'Test',
+                    'subject' => 'TYPO3 loves you - here is why',
+                    'template' => 'TestMailTemplate',
+                    'format' => FluidEmail::FORMAT_HTML,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider mailDataProvider
+     * @param array<string, string> $exampleMail
+     * @throws TransportExceptionInterface
+     */
+    public function testEmailEncoding(array $exampleMail): void
+    {
+        $this->createTestMail($exampleMail);
         $this->subject->run();
 
         $messages = $this->subject->getMessages();
 
-        self::assertEquals(
-            'https://domain/sign-in/registration/confirmation?tx_getaccess_confirmation%5Baction%5D=user&tx_getaccess_confirmation%5Bcontroller%5D=Confirmation&tx_getaccess_confirmation%5Bhash%5D=2720ba93a3007066&cHash=3179e61306999063853b2247a1bd4d',
-            $messages[0]->bodyPlain
-        );
+        if ($exampleMail['format'] === FluidEmail::FORMAT_HTML || $exampleMail['format'] === FluidEmail::FORMAT_BOTH) {
+            self::assertEmailFileEqualsString(
+                'EXT:xima_typo3_mailcatcher/Tests/Fixtures/' . $exampleMail['template'] . '.html',
+                $messages[0]->bodyHtml
+            );
+        }
+        if ($exampleMail['format'] === FluidEmail::FORMAT_PLAIN || $exampleMail['format'] === FluidEmail::FORMAT_BOTH) {
+            self::assertEmailFileEqualsString(
+                'EXT:xima_typo3_mailcatcher/Tests/Fixtures/' . $exampleMail['template'] . '.txt',
+                $messages[0]->bodyPlain
+            );
+        }
+        self::assertEquals($exampleMail['to'], $messages[0]->to);
+        self::assertEquals($exampleMail['toName'], $messages[0]->toName);
+        self::assertEquals($exampleMail['from'], $messages[0]->from);
+        self::assertEquals($exampleMail['fromName'], $messages[0]->fromName);
+        self::assertEquals($exampleMail['subject'], $messages[0]->subject);
     }
 
-    private function createTestMail(): void
+    /**
+     * @param array<string, string> $exampleMail
+     * @throws TransportExceptionInterface
+     */
+    private function createTestMail(array $exampleMail): void
     {
         $email = new FluidEmail();
         $email
-            ->to('contact@example.org')
-            ->from(new Address('jeremy@example.org', 'Jeremy'))
-            ->format(FluidEmail::FORMAT_BOTH)
-            ->subject('TYPO3 loves you - here is why')
+            ->to(new Address($exampleMail['to'], $exampleMail['toName']))
+            ->from(new Address($exampleMail['from'], $exampleMail['fromName']))
+            ->format($exampleMail['format'])
+            ->subject($exampleMail['subject'])
             ->setTemplate('TestMailTemplate');
         GeneralUtility::makeInstance(MailerInterface::class)->send($email);
+    }
+
+    public static function assertEmailFileEqualsString(string $emailPath, string $string, string $message = null): void
+    {
+        $emailPath = GeneralUtility::getFileAbsFileName($emailPath);
+        self::assertFileExists($emailPath);
+        $emailContent = file_get_contents($emailPath);
+        $emailContentWithoutBreaks = \trim(string: \preg_replace('/\s+/', ' ', $emailContent ?: '') ?? '');
+        $emailContentWithoutBreaks .= 'f';
+        if ($emailContentWithoutBreaks !== $string) {
+            file_put_contents(Environment::getVarPath() . '/log/' . md5($emailPath) . '_original.html', $string);
+            file_put_contents(Environment::getVarPath() . '/log/' . md5($emailPath) . '_fail.html', $emailContent);
+        }
+        self::assertEquals(
+            $emailContentWithoutBreaks,
+            $string,
+            ($message ?? '') . 'Messages have been saved to ' . Environment::getVarPath() . '/log/'
+        );
     }
 
     protected function setUp(): void
