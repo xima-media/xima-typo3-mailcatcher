72b1e664c466ccd2595287637c02e6e57d9b5886
dkranz <d.kranz@leuchtfeuer.com>
2025-10-09 11:51:05 +0200

    [BUGFIX] Fix module access

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -8,7 +8,7 @@ return $isMbox ? [
     'system_mails' => [
         'parent' => 'system',
         'position' => [],
-        'access' => 'user,group',
+        'access' => 'user',
         'iconIdentifier' => 'module-mailcatcher',
         'workspaces' => '*',
         'labels' => 'LLL:EXT:xima_typo3_mailcatcher/Resources/Private/Language/locallang_mod.xlf',
